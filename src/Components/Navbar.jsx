1b2f0f82c456d4f56a8dd923770b0435a251c75c
Mack Ramirez <mnrdev510@gmail.com>
2024-01-22 22:01:57 -0800

    adding this branch to correct my mistakes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,17 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDrowpdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image"
 
 const NavbarComp = () => {
   return (
     <Navbar>
       <Container>
         <Navbar.Brand>Brand Logo</Navbar.Brand>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" roundedCircle />
+        </Col>
       </Container>
     </Navbar>
   );
